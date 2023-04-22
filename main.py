ca29c7982fa0937fbe0993d323797e3148db1b76
yara hammami <yara.hammami13@gmail.com>
2023-04-22 07:51:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import json
+import boto3
+from algoliasearch.search_client import SearchClient
+
+algolia_client = SearchClient.create("ZLYFD8VG8C", "c82aa53030c57199a0f750626533080c")
+index = algolia_client.init_index("dev_test")
